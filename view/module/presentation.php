56b316124b44831aa3b5948a506085adc7a08815
jeager <jjmanco2@misena.edu.co>
2022-05-18 08:43:51 -0500

    sin view

--- view/module/presentation.php
+++
@@ -1,8 +0,0 @@
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <img src="view/img/imagen.png" width="100%" height="100%">
-
-    <h1> WELCOME TO SENA </h1>
-  </div>
-  <!-- /.content-wrapper -->
